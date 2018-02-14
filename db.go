9a0bb8730502623735bf0f81005900871045c8eb
alicegawa <alicegawa@sample.com>
2018-02-14 08:47:15 +0000

    implement PUT and GET method of pebblesdb wrapper
    following to the levigo implementation (almost same)

--- db.go
+++ db.go
@@ -1,5 +1,197 @@
 package pebigo
 
-// #cgo LDFLAGS: -lpebblesdb
-// #include "pebblesdb/c.h"
+/*
+#cgo LDFLAGS: -lpebblesdb
+#include <stdlib.h>
+#include "pebblesdb/c.h"
+
+// This function exists only to clean up lack-of-const warnings when
+// leveldb_approximate_sizes is called from Go-land.
+void pebigo_leveldb_approximate_sizes(
+    leveldb_t* db,
+    int num_ranges,
+    char** range_start_key, const size_t* range_start_key_len,
+    char** range_limit_key, const size_t* range_limit_key_len,
+    uint64_t* sizes) {
+  leveldb_approximate_sizes(db,
+                            num_ranges,
+                            (const char* const*)range_start_key,
+                            range_start_key_len,
+                            (const char* const*)range_limit_key,
+                            range_limit_key_len,
+                            sizes);
+}
+*/
 import "C"
+
+import (
+	"errors"
+	"unsafe"
+)
+
+type DatabaseError string
+
+func (e DatabaseError) Error() string {
+	return string(e)
+}
+
+var ErrDBClosed = errors.New("database is closed")
+
+// DB is a reusable handle to a LevelDB database on disk, created by Open.
+//
+// To avoid memory and file descriptor leaks, call Close when the process no
+// longer needs the handle. Calls to any DB method made after Close will
+// panic.
+//
+// The DB instance may be shared between goroutines. The usual data race
+// conditions will occur if the same key is written to from more than one, of
+// course.
+type DB struct {
+	Pdb *C.leveldb_t
+	closed bool
+}
+
+// Range is a range of keys in the database. GetApproximateSizes calls with it
+// begin at the key Start and end right before the key Limit.
+type Range struct {
+	Start []byte
+	Limit []byte
+}
+
+// Snapshot provides a consistent view of read operations in a DB.
+//
+// Snapshot is used in read operations by setting it on a
+// ReadOptions. Snapshots are created by calling DB.NewSnapshot.
+//
+// To prevent memory leaks and resource strain in the database, the snapshot
+// returned must be released with DB.ReleaseSnapshot method on the DB that
+// created it.
+type Snapshot struct {
+	snap *C.leveldb_snapshot_t
+}
+
+// Open opens a database.
+//
+// Creating a new database is done by calling SetCreateIfMissing(true) on the
+// Options passed to Open.
+//
+// It is usually wise to set a Cache object on the Options with SetCache to
+// keep recently used data from that database in memory.
+func Open(dbname string, o *Options) (*DB, error) {
+	var errStr *C.char
+	pdbname := C.CString(dbname)
+	defer C.free(unsafe.Pointer(pdbname))
+
+	pebblesdb := C.leveldb_open(o.Opt, pdbname, &errStr)
+	if errStr != nil {
+		gs := C.GoString(errStr)
+		C.leveldb_free(unsafe.Pointer(errStr))
+		return nil, DatabaseError(gs)
+	}
+	return &DB{pebblesdb, false}, nil
+}
+
+// DestroyDatabase removes a database entirely, removing everything from the
+// filesystem.
+func DestroyDatabase(dbname string, o *Options) error {
+	var errStr *C.char
+	pdbname := C.CString(dbname)
+	defer C.free(unsafe.Pointer(pdbname))
+
+	C.leveldb_destroy_db(o.Opt, pdbname, &errStr)
+	if errStr != nil {
+		gs := C.GoString(errStr)
+		C.leveldb_free(unsafe.Pointer(errStr))
+		return DatabaseError(gs)
+	}
+	return nil
+}
+
+// RepairDatabase attempts to repair a database.
+//
+// If the database is unrepairable, an error is returned.
+func RepairDatabase(dbname string, o *Options) error {
+	var errStr *C.char
+	pdbname := C.CString(dbname)
+	defer C.free(unsafe.Pointer(pdbname))
+
+	C.leveldb_repair_db(o.Opt, pdbname, &errStr)
+	if errStr != nil {
+		gs := C.GoString(errStr)
+		C.leveldb_free(unsafe.Pointer(errStr))
+		return DatabaseError(gs)
+	}
+	return nil
+}
+
+// Put writes data associated with a key to the database.
+//
+// If a nil []byte is passed in as value, it will be returned by Get
+// as an zero-length slice. The WriteOptions passed in can be reused
+// by multiple calls to this and if the WriteOptions is left unchanged.
+//
+// The key and value byte slices may be reused safely. Put takes a copy of
+// them before returning.
+func (db *DB) Put(wo *WriteOptions, key, value []byte) error {
+	if db.closed {
+		panic(ErrDBClosed)
+	}
+
+	var errStr *C.char
+	// leveldb_put, _get, and _delete call memcpy() (by way of Memtable::Add)
+	// when called, so we do not need to worry about these []byte being
+	// reclaimed by GC.
+	var k, v *C.char
+	if len(key) != 0 {
+		k = (*C.char)(unsafe.Pointer(&key[0]))
+	}
+	if len(value) != 0 {
+		v = (*C.char)(unsafe.Pointer(&value[0]))
+	}
+
+	lenk := len(key)
+	lenv := len(value)
+	C.leveldb_put(db.Pdb, wo.Opt, k, C.size_t(lenk), v, C.size_t(lenv), &errStr)
+	if errStr != nil {
+		gs := C.GoString(errStr)
+		C.leveldb_free(unsafe.Pointer(errStr))
+		return DatabaseError(gs)
+	}
+	return nil
+}
+
+// Get returns the data associated with the key from the database.
+//
+// If the key does not exist in the database, a nil []byte is returned. If the
+// key does exist, but the data is zero-length in the database, a zero-length
+// []byte will be returned.
+//
+// The key byte slice may be reused safely. Get takes a copy of
+// them before returning.
+func (db *DB) Get(ro *ReadOptions, key []byte) ([]byte, error) {
+	if db.closed {
+		panic(ErrDBClosed)
+	}
+
+	var errStr *C.char
+	var vallen C.size_t
+	var k *C.char
+	if len(key) != 0 {
+		k = (*C.char)(unsafe.Pointer(&key[0]))
+	}
+
+	value := C.leveldb_get(db.Pdb, ro.Opt, k, C.size_t(len(key)), &vallen, &errStr)
+
+	if errStr != nil {
+		gs := C.GoString(errStr)
+		C.leveldb_free(unsafe.Pointer(errStr))
+		return nil, DatabaseError(gs)
+	}
+
+	if value == nil {
+		return nil, nil
+	}
+
+	defer C.leveldb_free(unsafe.Pointer(value))
+	return C.GoBytes(unsafe.Pointer(value), C.int(vallen)), nil
+}
